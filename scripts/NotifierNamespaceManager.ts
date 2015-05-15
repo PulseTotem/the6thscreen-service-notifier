2221dd3aeb63c1f0515b3da7a5db0584f6f1fd61
Christian Brel <ch.brel@gmail.com>
2015-05-15 15:57:28 +0200

    refs SERVICES-31 : Enhance Notify Service.

--- scripts/NotifierNamespaceManager.ts
+++ scripts/NotifierNamespaceManager.ts
@@ -9,6 +9,10 @@
 
 /// <reference path="../t6s-core/core-backend/scripts/server/SourceNamespaceManager.ts" />
 
+/// <reference path="../t6s-core/core-backend/t6s-core/core/scripts/infotype/UserList.ts" />
+/// <reference path="../t6s-core/core-backend/t6s-core/core/scripts/infotype/User.ts" />
+/// <reference path="../t6s-core/core-backend/t6s-core/core/scripts/infotype/priorities/InfoPriority.ts" />
+
 var datejs : any = require('datejs');
 
 var DateJS : any = <any>Date;
@@ -53,8 +57,18 @@ class NotifierNamespaceManager extends SourceNamespaceManager {
 	 * @param {any} message - Received message
 	 */
 	onExternalMessage(from : string, message : any) {
-		if(from == "notify") {
-			//this.sendNewInfoToClient();//TODO
+		if(from == "notify" && typeof(message.username) != "undefined") {
+			var userList : UserList = new UserList();
+			userList.setId(uuid.v1());
+
+			var user = new User();
+			user.setId(uuid.v1());
+			user.setPriority(InfoPriority.HIGH);
+			user.setUsername(message.username);
+
+			userList.addUser(user);
+
+			this.sendNewInfoToClient(userList);
 		}
 	}
 }
