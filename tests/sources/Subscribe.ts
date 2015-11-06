d55d25cb7ca543616db1717bd7301e03aa3c728f
Simon Urli <simon.urli@gmail.com>
2015-11-06 10:18:45 +0100

    SERVICES-127 Fix test in service notifier

--- tests/sources/Subscribe.ts
+++ tests/sources/Subscribe.ts
@@ -8,7 +8,7 @@
 /// <reference path="../../scripts/sources/Subscribe.ts" />
 
 var assert = require("assert");
-var sinon : Sinon.SinonStatic = require("sinon");
+var sinon : SinonStatic = require("sinon");
 
 describe('Subscribe', function() {
 	describe('#constructor', function () {
